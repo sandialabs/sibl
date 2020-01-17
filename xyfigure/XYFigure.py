ca0e5be912183e4f544ef41f4f0cd54e6d00af67
hovey <chad.hovey@hoveyconsulting.com>
2020-01-17 15:34:12 -0700

    background image, custom tick marks

--- xyfigure/XYFigure.py
+++ xyfigure/XYFigure.py
@@ -9,6 +9,8 @@ from abc import ABC
 from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
+# import imageio
+from PIL import Image
 
 #from matplotlib import rc
 #rc('font', **{'family': 'serif', 'serif': ['Computer Modern Roman']})
@@ -80,6 +82,11 @@ class XYView(XYBase):
         self._title = kwargs.get('title', default)
         self._xlabel = kwargs.get('xlabel', 'default x axis label')
         self._ylabel = kwargs.get('ylabel', 'default y axis label')
+
+        self._xticks_str = kwargs.get('xticks', None)
+        self._yticks_str = kwargs.get('yticks', None)
+        # self._yticks_rhs_str = kwargs.get('yticks_rhs', None)
+
         self._x_log_scale = kwargs.get('x_log_scale', None)
 
         # default = {'scale': 1, 'label': 'ylabel_rhs', 'verification': 0}
@@ -88,6 +95,8 @@ class XYView(XYBase):
         # default value if figure_kwargs not client-supplied
         default = {'figsize': '(11.0, 8.5)'}  # inches, U.S. paper, landscape
         self._figure_args = kwargs.get('figure_args', default)
+
+        self._background_image = kwargs.get('background-image', None)
 
         self._display = kwargs.get('display', True)
         self._serialize = kwargs.get('serialize', False)
@@ -139,11 +148,16 @@ class XYView(XYBase):
             if self._yaxis_rhs:
                 rhs_axis_scale = self._yaxis_rhs.get('scale', 1)
                 rhs_axis_label = self._yaxis_rhs.get('label', None)
+                rhs_yticks_str = self._yaxis_rhs.get('yticks', None)
 
                 ax_rhs = fig.add_subplot(111, sharex=ax, frameon=False)
                 bottom, top = ax.get_ylim()
                 ax_rhs.set_ylim(rhs_axis_scale * bottom, rhs_axis_scale * top)
                 ax_rhs.yaxis.tick_right()
+                if rhs_yticks_str:
+                    ticks = eval(rhs_yticks_str)
+                    # ax_rhs.yaxis.yticks(ticks)
+                    plt.yticks(ticks)
                 ax_rhs.yaxis.set_label_position('right')
                 ax_rhs.set_ylabel(rhs_axis_label)
 
@@ -160,6 +174,35 @@ class XYView(XYBase):
             ax.set_ylabel(self._ylabel)
             ax.grid()
             ax.legend()
+
+            if self._xticks_str:
+                ticks = eval(self._xticks_str)
+                plt.xticks(ticks)
+
+            if self._yticks_str:
+                ticks = eval(self._yticks_str)
+                plt.yticks(ticks)
+
+            if self._background_image:
+                folder = self._background_image.get('folder', '.')
+                file = self._background_image.get('file', None)
+                path_and_file = os.path.join(folder, file)
+                # im = imageio.imread(path_and_file)
+                im = Image.open(path_and_file)
+
+                offset_tuple_str = self._background_image.get('offset', '0.0, 0.0')
+                dx, dy = eval(offset_tuple_str)  # offset x and y
+
+                scale_tuple_str = self._background_image.get('scale', '1.0, 1.0')
+                sx, sy = eval(scale_tuple_str)  # scale x and y
+                # extent = [left, right, top, bottom]
+                left, right = plt.xlim()
+                bottom, top = plt.ylim()
+                new_extent=[sx * left + dx, sx * right + dx, sy * bottom + dy, sy * top + dy]
+                ax.imshow(im, zorder=0, extent=new_extent)
+                a = 4
+
+
             if self._display:
                 plt.show()
             if self._serialize:
