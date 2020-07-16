f71bc0c719bf388542f151b522550d60c7c6a89c
Chad B. Hovey <chad.hovey@hoveyconsulting.com>
2020-07-16 10:16:02 -0600

    original and Luke's update injury curves

--- process/exodus/visualization.py
+++ process/exodus/visualization.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 
 EXEMPLAR = 0  # turn on or off the exemplar problem
+TEST = 1 # turn on or off Bob test with small data set
+INJURY_0 = 1  # turn on or off cellular injury curve, original
+INJURY_1 = 1  # updated Summey injury curves
 FIG_NAME = os.path.basename(__file__).split('.')[0]  # remove the .py extension
 FIG_FORMAT = 'pdf'
 LATEX = 1
@@ -56,61 +59,77 @@ else:
     ## Client application initializaton - end
     ## -------------------------------- ##
     
-    
-    ## -------------------------------- ##
-    ## User Input Deck, simulation-specific input - begin
-    
-    # block 7 is white matter is 504,505 data points 
-    # block 8 is gray matter is 790,102 data points
-    # combined white + gray = 1,294,607 data points
-    
-    # relative to this script, location of the particular simulation
-    simulation_path = '../../../casco_sim/bob-1mm-5kg-helmet2-0305-hemi-063f/'
+    if TEST:
+        simulation_path = '.' # here, in same location as visualization.py
+        idx = 0  # index for the probes
+        probes = {
+            "steps": [0],
+            "time": [0.00],
+            "strain_p95": [0.015],
+            "strain_rate_p95": [30]
+        }
+        axis_txt = f'time = {probes["time"][idx]*1000:.3f} ms (Bob-TEST-1000-pts)'
+        blocks = [7]
+        labels = ['white matter']
+        colors = ['C1']  # white plotted as orange, gray -> green
+        strain_files = [['test_ebe_max_principal_log_strain_51_small.txt']]
+        strain_rate_files = [['test_ebe_max_principal_rate_of_deformation_51_small.txt']]
+        marker_dict = {'linestyle': '', 'marker': '.', 'markersize': 10, 'alpha': 0.2}
 
-    # time_steps = [30, 51, 57]
-    # times = [0.0058, 0.010, 0.0112] # seconds
-    idx = 0  # index for the probes
-    probes = {
-        "steps": [30, 51, 57],
-        "time": [0.00580000428262166, 0.010000030740917116, 0.011200009903610695],
-        "strain_p95": [0.013038920686082887, 0.007864328738051788,0.009356105757136385],
-        "strain_rate_p95": [26.62451150429535, 45.64035758617126, 47.167653798895905]
-    }
+    else:
+        ## -------------------------------- ##
+        ## User Input Deck, simulation-specific input - begin
+    
+        # block 7 is white matter is 504,505 data points 
+        # block 8 is gray matter is 790,102 data points
+        # combined white + gray = 1,294,607 data points
+    
+        # relative to this script, location of the particular simulation
+        simulation_path = '../../../casco_sim/bob-1mm-5kg-helmet2-0305-hemi-063f/'
 
-    axis_txt = f'time = {probes["time"][idx]*1000:.3f} ms (Bob-063f)'
-    # strain_rate_txt = f'95% = {probes["strain_rate_p95"][idx]:.2f} /s' 
-    # strain_txt = f'95% = {probes["strain_p95"][idx]:.3f}'
+        # time_steps = [30, 51, 57]
+        # times = [0.0058, 0.010, 0.0112] # seconds
+        idx = 0  # index for the probes
+        probes = {
+            "steps": [30, 51, 57],
+            "time": [0.00580000428262166, 0.010000030740917116, 0.011200009903610695],
+            "strain_p95": [0.013038920686082887, 0.007864328738051788,0.009356105757136385],
+            "strain_rate_p95": [26.62451150429535, 45.64035758617126, 47.167653798895905]
+        }
 
-    blocks = [7, 8]
-    labels = ['white matter','gray matter']
-    colors = ['C1','C2']  # white plotted as orange, gray -> green
-    # strain_files = [
-    #     'ts_30_block_7_max_principal_green_lagrange_strain.txt','ts_30_block_8_max_principal_green_lagrange_strain.txt']
-    strain_files = [
-        [
-            'ts_30_block_7_max_principal_green_lagrange_strain.txt',
-            'ts_30_block_8_max_principal_green_lagrange_strain.txt'
-        ],[
-            'ts_51_block_7_max_principal_green_lagrange_strain.txt',
-            'ts_51_block_8_max_principal_green_lagrange_strain.txt'
-        ],[
-            'ts_57_block_7_max_principal_green_lagrange_strain.txt',
-            'ts_57_block_8_max_principal_green_lagrange_strain.txt'
+        axis_txt = f'time = {probes["time"][idx]*1000:.3f} ms (Bob-063f)'
+
+        blocks = [7, 8]
+        labels = ['white matter','gray matter']
+        colors = ['C1','C2']  # white plotted as orange, gray -> green
+
+        strain_files = [
+            [
+                'ts_30_block_7_max_principal_green_lagrange_strain.txt',
+                'ts_30_block_8_max_principal_green_lagrange_strain.txt'
+            ],[
+                'ts_51_block_7_max_principal_green_lagrange_strain.txt',
+                'ts_51_block_8_max_principal_green_lagrange_strain.txt'
+            ],[
+                'ts_57_block_7_max_principal_green_lagrange_strain.txt',
+                'ts_57_block_8_max_principal_green_lagrange_strain.txt'
+            ]
         ]
-    ]
 
-    strain_rate_files = [
-        [
-            'ts_30_block_7_max_principal_green_lagrange_strain_rate.txt',
-            'ts_30_block_8_max_principal_green_lagrange_strain_rate.txt'
-        ],[
-            'ts_51_block_7_max_principal_green_lagrange_strain_rate.txt',
-            'ts_51_block_8_max_principal_green_lagrange_strain_rate.txt'
-        ],[
-            'ts_57_block_7_max_principal_green_lagrange_strain_rate.txt',
-            'ts_57_block_8_max_principal_green_lagrange_strain_rate.txt'
+        strain_rate_files = [
+            [
+                'ts_30_block_7_max_principal_green_lagrange_strain_rate.txt',
+                'ts_30_block_8_max_principal_green_lagrange_strain_rate.txt'
+            ],[
+                'ts_51_block_7_max_principal_green_lagrange_strain_rate.txt',
+                'ts_51_block_8_max_principal_green_lagrange_strain_rate.txt'
+            ],[
+                'ts_57_block_7_max_principal_green_lagrange_strain_rate.txt',
+                'ts_57_block_8_max_principal_green_lagrange_strain_rate.txt'
+            ]
         ]
-    ]
+
+        marker_dict = {'linestyle': '', 'marker': ',', 'markersize': 0.7, 'alpha': 0.2}
 
     ## User Input Deck, simulation-specific input - end
     ## -------------------------------- ##
@@ -130,7 +149,8 @@ else:
         strain_rate = np.concatenate((strain_rate,  block_strain_rate))
     
     g = sns.JointGrid(strain_rate, strain)
-    g = g.plot_joint(plt.plot, linestyle='', marker=',', markersize=0.7, alpha=0.2)
+    # g = g.plot_joint(plt.plot, linestyle='', marker=',', markersize=0.7, alpha=0.2)
+    g = g.plot_joint(plt.plot, **marker_dict)
 
     exp_min = -1  # x-domain minimum 10^exp_min
     exp_max = 3  # x-domain maximum 10^exp_max
@@ -184,6 +204,31 @@ else:
 
     plt.xlabel("max(eig(GL strain rate)) (1/s)")
     plt.ylabel("max(eig(GL strain)) (cm/cm)")
+
+    if INJURY_0 or INJURY_1:
+        exp_min = -2  # x-domain minimum 10^exp_min
+        exp_max = 4  # x-domain maximum 10^exp_max
+        npts = 100  # number of points
+        # x = np.linspace(-4, 4, npts)
+        x = np.logspace(exp_min, exp_max, npts)
+
+        # injury curves
+        if INJURY_0:
+            # pathway-induced injury
+            # y_pathway = 0.2589 * np.arctan(-0.5789 * np.log(10**x) - 1.83) + 0.4192
+            y_pathway = 0.2589 * np.arctan(-0.5789 * np.log(x) - 1.83) + 0.4192
+            # mechanical injury
+            # y_mechanical = 0.345 * np.arctan(-0.2923 * np.log(10**x) - 0.1617) + 0.5033
+            y_mechanical = 0.345 * np.arctan(-0.2923 * np.log(x) - 0.1617) + 0.5033
+    
+            g.ax_joint.plot(x, y_pathway, linestyle='--', color='green', linewidth=4, alpha=0.8, label='pathway induced injury')
+            g.ax_joint.legend()
+
+        if INJURY_1:
+            y_cell_death = 0.128 * x**(-0.156)
+            g.ax_joint.plot(x, y_cell_death, linestyle='--', color='black', linewidth=4, alpha=0.8, label='cell death')
+            g.ax_joint.legend()
+
 
     plt.show()
 
