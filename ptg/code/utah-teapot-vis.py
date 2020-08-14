bade42a18908dd7ddde81ee985d0311a6020e6fb
Chad B. Hovey <chad.hovey@hoveyconsulting.com>
2020-08-14 17:28:50 -0600

    visualize patches and control point numbers

--- ptg/code/utah-teapot-vis.py
+++ ptg/code/utah-teapot-vis.py
@@ -1,66 +1,12 @@
 # utah teapot visualization
 import argparse
-import csv
 import json
 from pathlib import Path
 import sys
 
+import matplotlib.pyplot as plt
 import numpy as np
 
-# functional programming, named tuple, Dan Bader, Real Python
-# https://realpython.com/lessons/immutable-data-structures-namedtuple/
-# import collections
-
-# csv file to namedtuple
-# https://stackoverflow.com/questions/9007174/what-is-the-pythonic-way-to-read-csv-file-data-as-rows-of-namedtuples
-
-
-# from collections import namedtuple
-
-# input file begin
-# file_control_points = 'utah-teapot-coordinates.csv'
-# data_type_string = 'float'
-# data_type_delimiter = ','
-# n_headers = 0  # no headers in the file_control_points csv file
-# 
-# # input file end
-# 
-# # visualize control points
-# 
-# # tea pot bottom
-# 
-# # named tuple, a named tuple instance is immutable
-# # ControlPoint = collections.namedtuple('ControlPoint', ['x', 'y', 'z'])
-# # ControlPoint = namedtuple('ControlPoint', ['x', 'y', 'z'])
-# 
-# control_points_list = []  # this feel wrong b/c it is mutable
-# 
-# with open(file_control_points, mode='rt') as fin:
-#     reader = csv.reader(fin)
-#     # ControlPoint = namedtuple("ControlPoint", next(reader))
-#     ControlPoint = namedtuple("ControlPoint", ['x', 'y', 'z'])
-#     for data in map(ControlPoint._make, reader):
-#         print(data.x)
-#         control_points_list.append(ControlPoint)
-# 
-# 
-# control_points = tuple(control_points_list)
-# 
-# a = 4
-
-# with open(file_control_points) as fin:
-#     data = np.genfromtxt(file_control_points,
-#                          dtype=data_type_string,
-#                          delimiter=data_type_delimiter, 
-#                          skip_header=n_headers)
-
-# tea pot original (without bottom)
-
-# label control points
-
-# visualize low-frequency Bezier surface
-
-# visualize high-frequency Bezier surface
 
 class BezierSurfaceVis:
 
@@ -86,18 +32,6 @@ class BezierSurfaceVis:
             if not key:
                 sys.exit(f'Error: keyword "{kw}" not found in config file.')
 
-        # data_path = db.get("data-path", None)
-        # if not data_path:
-        #     sys.exit('Error: no data path specified.')
-
-        # cp_file = db.get("control-points", None)
-        # if not cp_file:
-        #     sys.exit('Error: no control points file specified.')
-
-        # co_file = db.get("connections", None)
-        # if not co_file:
-        #     sys.exit('Error: no connections file specified.')
-
         data_path = db.get("data-path", None)
         cp_file = db.get("control-points", None)
         co_file = db.get("connections", None)
@@ -118,7 +52,6 @@ class BezierSurfaceVis:
         data_type_delimiter = ','
         n_headers = 0  # no headers in the csv files
 
-        # cp_x, cp_y, cp_z = (None, None, None)
         idx, idy, idz = (0, 1, 2)  # column numbers from .csv file
 
         with open(cp_path_file) as fin:
@@ -138,6 +71,50 @@ class BezierSurfaceVis:
 
         a = 4
 
+        ax = plt.axes(projection='3d')
+        # ax.plot3D(cp_x, cp_y, cp_z)
+        ax.scatter3D(cp_x, cp_y, cp_z)
+
+        # for patch in patches:
+        # patch number, description
+        # 0, rim
+        # 1, body
+        # 2, body continued
+        # 3, lid
+        # 4, lid continued
+        # 5, handle
+        # 6, handle continued
+        # 7, spout
+        # 8, spout continued
+        # 9, bottom
+        cp_labels = True  # show/hide control point index
+        # patch_number = 6
+        # for patch in [patches[patch_number]]:
+        patch_numbers = (5, 6)
+        for patch in [patches[k] for k in patch_numbers]:
+            patch_x = [cp_x[i] for i in patch]
+            patch_y = [cp_y[i] for i in patch]
+            patch_z = [cp_z[i] for i in patch]
+            ax.plot3D(patch_x, patch_y, patch_z, linestyle='dashed')
+
+            if cp_labels:
+                for i, cp_index in enumerate(patch):
+                    if verbose:
+                        print(f'patch node {i} is control point {cp_index}')
+                    ax.text(patch_x[i], patch_y[i], patch_z[i],
+                            str(cp_index), color='black')
+                    a = 4
+
+
+        # ax.set_xlabel(r'$t$')
+        ax.set_xlabel(r'$x$')
+        ax.set_ylabel(r'$y$')
+        ax.set_zlabel(r'$z$')
+
+        plt.show()
+
+
+        b = 4
 
 
 def main(argv):
