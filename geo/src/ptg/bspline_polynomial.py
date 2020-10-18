a07b128d7041d7b7a279b00c7f6bdcfdde2e4041
hovey <hovey@me.com>
2020-10-18 16:10:58 -0600

    enhanced error check of bspline

--- geo/src/ptg/bspline_polynomial.py
+++ geo/src/ptg/bspline_polynomial.py
@@ -4,28 +4,44 @@ import numpy as np
 
 
 def bspline_polynomial(
-    knot_vector: list, knot_k: int, p: int = 0, nti: int = 2, verbose: bool = False
+    knot_vector: list, knot_k: int = 0, p: int = 0, nti: int = 1, verbose: bool = False
 ):
     """Computes the B-spline polynomial basis
 
     Args:
         knot_vector (float array): [t0, t1, t2, ... tK] of length (K+1), and K knot spans
+            must have length of 2 or more
+            must be a non-decreasing sequence
         knot_k (int): index in the list of possible knot_index values = [0, 1, 2, ... K]
         p (int): polynomial degree (p=0: constant, p=1: linear, p=2: quadratic, p=3: cubic, etc.)
-        nti (int): number of time intervals for t in per knot span [t_k, t_{k+1}], nti = 2 is default
+        nti (int): number of time intervals for t in per knot span [t_k, t_{k+1}], nti = 1 is default
         verbose (bool): prints polynomial or error checking
 
     Returns:
-        tuple: arrays of (t, f(t)) as time t and polynomial evaluated at t.
+        tuple: arrays of (t, f(t)) as time t and polynomial evaluated at t; or, 
+        AssertionError: if input is out of range
     """
 
     num_knots = len(knot_vector)
+    MAX_DEGREE = 0
 
-    if knot_k >= 0 and knot_k <= (num_knots - 1) and p >= 0 and nti >= 2:
+    try:
+        assert len(knot_vector) >= 2, "Error: knot vector length must be two or larger."
+        assert knot_k >= 0, "Error: knot index knot_k must be non-negative."
+        assert p >= 0, "Error: polynomial degree p must be non-negative."
+        assert (
+            p <= MAX_DEGREE
+        ), f"Error: polynomial degree p exceeds maximum of {MAX_DEGREE}"
+        assert nti >= 1, "Error: number of time intervals nti must be 1 or greater."
+        assert knot_k <= (
+            num_knots - 1
+        ), "Error: knot index knot_k exceeds knot vector length plus 1."
+
         knots_lhs = knot_vector[0:-1]  # left-hand-side knot values
         knots_rhs = knot_vector[1:]  # right-hand-side knot values
         knot_spans = np.array(knots_rhs) - np.array(knots_lhs)
         dt = knot_spans / nti
+        assert all([dti >= 0 for dti in dt]), "Error: knot vector is decreasing."
 
         # improve index notation
         # t = [knots_lhs[i] + k * dt[i] for i in np.arange(num_knots-1) for k in np.arange(nti)]
@@ -54,18 +70,63 @@ def bspline_polynomial(
             if verbose:
                 print(f"t = {t}")
                 print(f"y = {y}")
-            return t, y
-        else:
-            print("Not implemented for p>0.")
-            return None
 
-    else:
+        return t, y
+        # else:
+        #     print("Not implemented for p>0.")
+        #     return None
+
+    except AssertionError as error:
         if verbose:
-            print("Input is out of range.")
-            print("Knot index i must be:")
-            print("  non-negative, and be")
-            print("  less than index of last knot vector.")
-            print("Polynomial degree p must be a non-negative integer.")
-            print("Number of time intervals nti >= 2.")
+            print(error)
 
-        return None
+        return error
+
+    # if knot_k >= 0 and knot_k <= (num_knots - 1) and p >= 0 and nti >= 1:
+    #     knots_lhs = knot_vector[0:-1]  # left-hand-side knot values
+    #     knots_rhs = knot_vector[1:]  # right-hand-side knot values
+    #     knot_spans = np.array(knots_rhs) - np.array(knots_lhs)
+    #     dt = knot_spans / nti
+
+    #     # improve index notation
+    #     # t = [knots_lhs[i] + k * dt[i] for i in np.arange(num_knots-1) for k in np.arange(nti)]
+    #     t = [
+    #         knots_lhs[k] + j * dt[k]
+    #         for k in np.arange(num_knots - 1)
+    #         for j in np.arange(nti)
+    #     ]
+    #     t.append(knot_vector[-1])
+    #     t = np.array(t)
+
+    #     y = np.zeros((num_knots - 1) * nti + 1)
+
+    #     if verbose:
+    #         print(f"Knot vector: {knot_vector}")
+    #         print(f"Number of knots = {num_knots}")
+    #         print(f"Knot index: {knot_k}")
+    #         print(f"Left-hand-side knot vector values: {knots_lhs}")
+    #         print(f"Right-hand-side knot vector values: {knots_rhs}")
+    #         print(f"Knot spans: {knot_spans}")
+    #         print(f"Number of time intervals per knot span: {nti}")
+    #         print(f"Knot span deltas: {dt}")
+
+    #     if p == 0:
+    #         y[knot_k * nti : knot_k * nti + nti] = 1.0
+    #         if verbose:
+    #             print(f"t = {t}")
+    #             print(f"y = {y}")
+    #         return t, y
+    #     else:
+    #         print("Not implemented for p>0.")
+    #         return None
+
+    # else:
+    #     if verbose:
+    #         print("Input is out of range.")
+    #         print("Knot index i must be:")
+    #         print("  non-negative, and be")
+    #         print("  less than index of last knot vector.")
+    #         print("Polynomial degree p must be a non-negative integer.")
+    #         print("Number of time intervals nti >= 2.")
+
+    #     return None
--- geo/tests/test_bspline_polynomial.py
+++ geo/tests/test_bspline_polynomial.py
@@ -9,8 +9,8 @@ $ black --check geo/tests/test_bspline_polynomial.py
 $ pytest geo/tests/test_bspline_polynomial.py -v
 $ pytest geo/tests/test_bspline_polynomial.py -v --cov=geo/src/ptg --cov-report term-missing
 """
-# from unittest import TestCase, main
-from unittest import TestCase
+# from unittest import TestCase
+from unittest import TestCase, main
 
 import numpy as np
 
@@ -21,92 +21,135 @@ import ptg.bspline_polynomial as bp
 class TestBspline(TestCase):
     @classmethod
     def setUpClass(cls):
-        cls._TOL = 1e-6  # tolerance
-        cls._nti = 4  # number of time intervals
-        # t, e.g., two intervals, three evaluation points
-        cls._verbosity = True
+        cls.TOL = 1e-6  # tolerance
+        cls.nti = 4  # number of time intervals per knot
+        # t, e.g., nti=1 gives two sub-intervals, three evaluation points
+        cls.verbosity = False
+        cls.kv = [0, 2, 3]  # list, knot_vector
+        cls.ki = 0  # non-negative integer, defaults to first index
+        cls.degree = 0  # non-negative polynomial degree, defaults to 0 (constant)
 
     @classmethod
     def same(cls, a, b):
         same_to_tolerance = False
         l2norm_diff = np.linalg.norm(a - b)
 
-        if cls._verbosity:
+        if cls.verbosity:
             print(f"array a = {a}")
             print(f"array b = {b}")
             print(f"l2norm_diff = {l2norm_diff}")
 
-        if np.abs(l2norm_diff) < cls._TOL:
+        if np.abs(l2norm_diff) < cls.TOL:
             same_to_tolerance = True
 
         return same_to_tolerance
 
-    def test_000_input_out_of_range_and_verbose(self):
-        knot_vector = [0, 2, 3]  # list
+    def test_001_knot_vector_minimum_length(self):
+        kv_too_short = [0]
+        knot_index = 0
+        calc = bp.bspline_polynomial(kv_too_short)
+        self.assertIsInstance(calc, AssertionError)
+        self.assertTrue(
+            calc.args[0] == "Error: knot vector length must be two or larger."
+        )
+
+    def test_002_knot_index_too_low(self):
+        bad_knot_index = -1
+        calc = bp.bspline_polynomial(self.kv, bad_knot_index)
+        self.assertIsInstance(calc, AssertionError)
+        self.assertTrue(
+            calc.args[0] == "Error: knot index knot_k must be non-negative."
+        )
+
+    def test_003_degree_too_small(self):
         degree = -1  # integer >= 0, so -1 is out of range for test
-
-        knot_index = 0  # integer >= 0
-
-        verbose = True
-
-        calc = bp.bspline_polynomial(
-            knot_vector, knot_index, degree, self._nti, verbose=verbose
+        calc = bp.bspline_polynomial(self.kv, self.ki, degree)
+        self.assertIsInstance(calc, AssertionError)
+        self.assertTrue(
+            calc.args[0] == "Error: polynomial degree p must be non-negative."
         )
-        self.assertIsNone(calc)
 
-    def test_000_p0(self):
-        knot_vector = [0, 2, 3]  # list
-        degree = 0  # integer >= 0
-
-        knot_index = 0  # integer >= 0
-        calc = bp.bspline_polynomial(
-            knot_vector, knot_index, degree, self._nti, self._verbosity
+    def test_004_degree_too_large(self):
+        degree = 1  # integer >= 0, but linear degree not yet implemented
+        calc = bp.bspline_polynomial(self.kv, self.ki, degree)
+        self.assertIsInstance(calc, AssertionError)
+        self.assertTrue(
+            calc.args[0] == "Error: polynomial degree p exceeds maximum of 0"
         )
-        if self._verbosity:
-            print(f"output = {calc}")
 
-        known_t = [0, 0.5, 1, 1.5, 2, 2.25, 2.5, 2.75, 3]
-        known_y = [1, 1.0, 1, 1.0, 0, 0.00, 0.0, 0.00, 0]
+    def test_005_number_of_time_intervals(self):
+        bad_number_of_intervals = 0
+        calc = bp.bspline_polynomial(
+            self.kv, self.ki, self.degree, bad_number_of_intervals
+        )
+        self.assertIsInstance(calc, AssertionError)
+        self.assertTrue(
+            calc.args[0] == "Error: number of time intervals nti must be 1 or greater."
+        )
 
+    def test_006_nti_out_of_range_and_verbose(self):
+        bad_number_of_intervals = 0
+        calc = bp.bspline_polynomial(
+            self.kv, self.ki, self.degree, bad_number_of_intervals, verbose=True
+        )
+        self.assertIsInstance(calc, AssertionError)
+        self.assertTrue(
+            calc.args[0] == "Error: number of time intervals nti must be 1 or greater."
+        )
+
+    def test_007_knot_index_too_high(self):
+        # Python's zero-based index makes the integer length of the knot_vector
+        # be the first index to be out of range, thus test this
+        bad_knot_index = len(self.kv)
+        calc = bp.bspline_polynomial(self.kv, bad_knot_index)
+        self.assertIsInstance(calc, AssertionError)
+        self.assertTrue(
+            calc.args[0]
+            == "Error: knot index knot_k exceeds knot vector length plus 1."
+        )
+
+    def test_008_decreasing_knots(self):
+        # knot vector must be non-decreasing sequence, so test error
+        # checking for a decreasing knot vector sequence
+        bad_knot_vector = [0, 2, 1]
+        calc = bp.bspline_polynomial(bad_knot_vector)
+        self.assertIsInstance(calc, AssertionError)
+        self.assertTrue(calc.args[0] == "Error: knot vector is decreasing.")
+
+    def test_N00_and_verbose(self):
+        calc = bp.bspline_polynomial(
+            self.kv, self.ki, self.degree, self.nti, verbose=True
+        )
+        known_t = [0, 0.5, 1, 1.5, 2, 2.25, 2.5, 2.75, 3]  # nti = 2
+        known_y = [1, 1, 1, 1, 0, 0, 0, 0, 0]
         self.assertTrue(self.same(known_t, calc[0]))
         self.assertTrue(self.same(known_y, calc[1]))
 
+    def test_N10(self):
         knot_index = 1  # integer >= 0
-        calc = bp.bspline_polynomial(
-            knot_vector, knot_index, degree, self._nti, self._verbosity
-        )
-        if self._verbosity:
-            print(f"output = {calc}")
-
-        known_t = [0, 0.5, 1, 1.5, 2, 2.25, 2.5, 2.75, 3]
+        calc = bp.bspline_polynomial(self.kv, knot_index, self.degree, self.nti)
+        known_t = [0, 0.5, 1, 1.5, 2, 2.25, 2.5, 2.75, 3]  # nti = 2
         known_y = [0, 0, 0, 0, 1, 1, 1, 1, 0]
-
         self.assertTrue(self.same(known_t, calc[0]))
         self.assertTrue(self.same(known_y, calc[1]))
 
+    def test_N20(self):
         knot_index = 2  # integer >= 0
-        calc = bp.bspline_polynomial(
-            knot_vector, knot_index, degree, self._nti, self._verbosity
-        )
-        if self._verbosity:
-            print(f"output = {calc}")
-
-        known_t = [0, 0.5, 1, 1.5, 2, 2.25, 2.5, 2.75, 3]
+        calc = bp.bspline_polynomial(self.kv, knot_index, self.degree, self.nti)
+        known_t = [0, 0.5, 1, 1.5, 2, 2.25, 2.5, 2.75, 3]  # nti = 2
         known_y = [0, 0, 0, 0, 0, 0, 0, 0, 1]
-
         self.assertTrue(self.same(known_t, calc[0]))
         self.assertTrue(self.same(known_y, calc[1]))
 
-    def test_001_p1_not_yet_implemented(self):
-        knot_vector = [0, 2, 3]  # list
+    def test_N01_not_yet_implemented(self):
         degree = 1  # integer >= 0
-
-        knot_index = 0  # integer >= 0
-        calc = bp.bspline_polynomial(
-            knot_vector, knot_index, degree, self._nti, self._verbosity
+        calc = bp.bspline_polynomial(self.kv, self.ki, degree)
+        self.assertIsInstance(calc, AssertionError)
+        self.assertTrue(
+            calc.args[0] == "Error: polynomial degree p exceeds maximum of 0"
         )
-        self.assertIsNone(calc)
 
 
-# if __name__ == "__main__":
-#     main()  # calls unittest.main()
+# retain main for debugging this file in VS code
+if __name__ == "__main__":
+    main()  # calls unittest.main()
