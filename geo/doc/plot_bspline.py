95df203cd052b50260f2511a2bfec28aa124063d
Chad B. Hovey <chad.hovey@hoveyconsulting.com>
2020-10-27 12:33:52 -0600

    reproduce Cottrell Hughes results

--- geo/doc/plot_bspline.py
+++ geo/doc/plot_bspline.py
@@ -12,15 +12,59 @@ import ptg.bspline as bsp
 
 # (siblenv) [~/sibl/geo/doc] python plot_bspline.py
 
-DEGREE = 2  # 0 constant, 1 linear, 2 quadratic, 3 cubic
-DISPLAY = 1
-DPI = 100  # dots per inch
-KNOT_OFFSET = 0
-LATEX = 0
-NBI = 5  # number of bisections per knot interval
-NCP = 9  # number of control points
-SERIALIZE = 0
-VERBOSE = 0
+config_recover_Bezier_linear = {
+    "degree": 1,
+    "ncp": 2,
+}
+
+config_recover_Bezier_quadratic = {
+    "degree": 2,
+    "nbi": 7,
+    "ncp": 3,
+}
+
+config_recover_Bezier_cubic = {
+    "degree": 3,
+    "nbi": 7,
+    "ncp": 4,
+}
+
+config_recover_Bezier_quartic = {
+    "degree": 4,
+    "nbi": 7,
+    "ncp": 5,
+}
+
+config_Cottrell_Fig2p5 = {
+    "degree": 2,
+    "nbi": 7,
+    "ncp": 8,
+    "knot_vector": [0, 0, 0, 1, 2, 3, 4, 4, 5, 5, 5],
+}
+
+config_Cottrell_Fig2p6 = {
+    "degree": 4,
+    "nbi": 7,
+    "ncp": 15,
+    "knot_vector": [0, 0, 0, 0, 0, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5],
+}
+
+# config = config_recover_Bezier_linear
+# config = config_recover_Bezier_quadratic
+# config = config_recover_Bezier_cubic
+# config = config_recover_Bezier_quartic
+# config = config_Cottrell_Fig2p5
+config = config_Cottrell_Fig2p6
+
+DEGREE = config.get("degree", 0)  # 0 constant, 1 linear, 2 quadratic, 3 cubic
+DISPLAY = config.get("display", True)
+DPI = config.get("dpi", 100)  # dots per inch
+KNOT_OFFSET = config.get("knot_offset", 0)  # translate knot vector to left or right
+LATEX = config.get("latex", False)
+NBI = config.get("nbi", 2)  # number of bisections per knot interval
+NCP = config.get("ncp", 2)  # number of control points
+SERIALIZE = config.get("serialize", False)  # save figure to disc
+VERBOSE = config.get("verbose", False)
 
 linestyles = ["solid", "dashed", "dashdot"]
 num_linestyles = len(linestyles)
@@ -29,49 +73,46 @@ if LATEX:
     rc("font", **{"family": "serif", "serif": ["Computer Modern Roman"]})
     rc("text", usetex=True)
 
-# e.g., p=0 constant, p=1 linear, p=2 quadratic, p=3 cubic
-
-# knot_vector = [-1, 0, 1, 2, 3, 4, 5]
-# knot_vector = [0, 1, 2, 3, 4, 5, 6]
 a, b = 0, NCP - DEGREE
 knot_vector = (
     np.concatenate((np.repeat(a, DEGREE), np.arange(a, b), np.repeat(b, DEGREE + 1)))
     + KNOT_OFFSET
 )
+KV = config.get("knot_vector", knot_vector)
 
 # number of elements is the number of non-zero knot spans
-num_elements = len(np.unique(knot_vector)) - 1
+num_elements = len(np.unique(KV)) - 1
 
 print(f"Computing B-spline with bases of degree={DEGREE}")
-print(f"with knot vector {knot_vector}")
+print(f"with knot vector {KV}")
 print(f"with number of bisections per knot interval={NBI}")
 print(f"with number of elements (non-zero knot spans)={num_elements}")
 
-knots_lhs = knot_vector[0:-1]  # left-hand-side knot values
-knots_rhs = knot_vector[1:]  # right-hand-side knot values
+knots_lhs = KV[0:-1]  # left-hand-side knot values
+knots_rhs = KV[1:]  # right-hand-side knot values
 knot_spans = np.array(knots_rhs) - np.array(knots_lhs)
 dt = knot_spans / (2 ** NBI)
 assert all([dti >= 0 for dti in dt]), "Error: knot vector is decreasing."
 
-num_knots = len(knot_vector)
+num_knots = len(KV)
 t = [
     knots_lhs[k] + j * dt[k]
     for k in np.arange(num_knots - 1)
     for j in np.arange(2 ** NBI)
 ]
-t.append(knot_vector[-1])
+t.append(KV[-1])
 t = np.array(t)
 N = []
 
-# for k in np.arange(len(knot_vector) - 1 - DEGREE):
+# for k in np.arange(len(KV) - 1 - DEGREE):
 for i in np.arange(NCP):
 
-    # coef = np.zeros(len(knot_vector) - (DEGREE + 1))
+    # coef = np.zeros(len(KV) - (DEGREE + 1))
     coef = np.zeros(NCP)
     # coef[k] = 1.0
     coef[i] = 1.0
 
-    B = bsp.BSpline(knot_vector, coef, DEGREE)
+    B = bsp.BSpline(KV, coef, DEGREE)
 
     if B.is_valid():
         y = B.evaluate(t)
@@ -89,12 +130,14 @@ ax.grid(True, which="minor", linestyle=":")
 
 # ax.plot(t, y, linestyle="None", marker=".")
 for i in np.arange(NCP):
+    CPTXT = f"{i}"
+    DEGTXT = f"{DEGREE}"
     ax.plot(
         t,
         N[i],
         "-",
         lw=2,
-        label=f"$N_{i}^{DEGREE}$",
+        label="$N_{" + CPTXT + "}^{" + DEGTXT + "}$",
         linestyle=linestyles[np.remainder(i, num_linestyles)],
     )
 
@@ -103,18 +146,14 @@ ax.set_xlabel(r"$t$")
 ax.set_ylabel(f"$N^{DEGREE}_i(t)$")
 
 eps = 0.1
-ax.set_xlim([knot_vector[0] - 2 * eps, knot_vector[-1] + 2 * eps])
-# ax.set_xlim([0.0 - 2 * eps, 6.0 + 2 * eps])
+ax.set_xlim([KV[0] - 2 * eps, KV[-1] + 2 * eps])
 ax.set_ylim([0.0 - 2 * eps, 1.0 + 2 * eps])
 ax.set_aspect("equal")
 ax.legend(bbox_to_anchor=(1.05, 1), loc="upper left", borderaxespad=0.0)
-# ax.xaxis.set_minor_locator(MultipleLocator(0.5))
 ax.xaxis.set_major_locator(MultipleLocator(1.0))
-ax.xaxis.set_minor_locator(MultipleLocator(0.5))
-# ax.xaxis.set_major_locator(MultipleLocator(1.0))
+ax.xaxis.set_minor_locator(MultipleLocator(0.25))
 ax.yaxis.set_major_locator(MultipleLocator(1.0))
-ax.yaxis.set_minor_locator(MultipleLocator(0.5))
-# ax.set_xticklabels(["1", "", "2"])
+ax.yaxis.set_minor_locator(MultipleLocator(0.25))
 
 
 if DISPLAY:
@@ -123,6 +162,6 @@ if DISPLAY:
 if SERIALIZE:
     extension = ".pdf"  # or '.svg'
     # bstring = "N(p=" + str(DEGREE) + ")_" + str(k) + extension
-    bstring = "N(p=" + str(DEGREE) + ")_i" + extension
+    bstring = "N(p=" + str(DEGREE) + ")_NCP=" + str(NCP) + extension
     # fig.savefig(bstring, bbox_inches="tight")
     fig.savefig(bstring, bbox_inches="tight", pad_inches=0)
