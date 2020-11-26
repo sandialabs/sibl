1bedc9c838611208cd3598ab69c2831436240a04
hovey <hovey@me.com>
2020-11-26 11:16:34 -0700

    try to pass server Black

--- geo/tests/test_bspline.py
+++ geo/tests/test_bspline.py
@@ -168,7 +168,8 @@ class TestBSpline(TestCase):
 
     def test_101_Bingol_2D_curve(self):
         """See 
-        https://nurbs-python.readthedocs.io/en/latest/visualization.html#curves"""
+        https://nurbs-python.readthedocs.io/en/latest/visualization.html#curves
+        """
 
         # knot vector
         KV = [0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.0, 1.0, 1.0, 1.0]
