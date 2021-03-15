5d53bf2168c8d2525bcd3f51e1419317cd0ae3e3
Chad Hovey <chovey@sandia.gov>
2021-03-15 09:37:42 -0600

    move from SIBL to PostPro GitLab

--- gui/src/zplot/assets/__init__.py
+++
@@ -1,4 +0,0 @@
-# This is a blank file used to allow import from subdirectories.
-# References:
-# https://stackoverflow.com/questions/1260792/import-a-file-from-a-subdirectory
-# https://packaging.python.org/tutorials/packaging-projects/#a-simple-project
--- gui/src/zplot/controls.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa
-
-# In[]:
-# Controls for webapp
-SIGNAL_PROC_TYPES = {
-    "BW": "Butterworth filter",
-    "DIFF": "Differentiation",
-}
