760550b374ae2fe9ab63f98caf92f123a719d820
Chad Hovey <chovey@sandia.gov>
2021-04-07 14:55:06 -0600

    harmonize with other bspline surface examples

--- geo/examples/bspline_surface_Bezier_recovery.py
+++ geo/examples/bspline_surface_Bezier_recovery.py
@@ -8,51 +8,93 @@ Example:
 """
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rc
 import matplotlib.tri as mtri
+from pathlib import Path
 
 import ptg.bspline as bsp
+import ptg.view_bspline as vbsp
 
 # Utilites
 ix, iy, iz = 0, 1, 2  # xyz indicies, avoid magic numbers
+control_net_shown = True  # True lets control net be drawn, False skips it
+control_points_shown = True
+serialize = True
+latex = True
+if latex:
+    rc("font", **{"family": "serif", "serif": ["Computer Modern Roman"]})
+    rc("text", usetex=True)
 
 # The first and only figure
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
-ax.set_xlabel("x")
-ax.set_ylabel("y")
-ax.set_zlabel("z")
+ax.set_xlabel(r"$x$")
+ax.set_ylabel(r"$y$")
+ax.set_zlabel(r"$z$")
+
+xmin, nsamples, xmax = 0.0, 5, 1.0
+interval = np.linspace(xmin, xmax, num=nsamples, endpoint=True)
+
+ax.set_xticks(interval)
+ax.set_yticks(interval)
+ax.set_zticks(interval)
 
 # The first and only Bspline surface
 kv_t = (0.0, 0.0, 1.0, 1.0)  # knot vector for t parameter
 kv_u = (0.0, 0.0, 1.0, 1.0)  # knot vector for u parameter
+degree_t = 1  # linear
+degree_u = 1  # linear
+nbi = 3  # number of bisections per knot interval
+
 control_points = (
     ((0.0, 0.0, 1.0), (0.0, 1.0, 0.0)),
     ((1.0, 0.0, 0.0), (1.0, 1.0, 0.0)),
 )
-degree_t = 1  # linear
-degree_u = 1  # linear
-nbi = 2  # number of bisections per knot interval
 
-S = bsp.Surface(
-    kv_t, kv_u, control_points, degree_t, degree_u, n_bisections=nbi, verbose=True,
-)
-(surf_x, surf_y, surf_z) = S.evaluations
+surfaces = (control_points,)
 
-S_evaluations_view_config = dict(
-    alpha=0.9, color="navy", linestyle="dashed", linewidth=0, marker=".", markersize=4
-)
+if control_net_shown:
+    pass
 
-ax.plot3D(
-    surf_x.flatten(), surf_y.flatten(), surf_z.flatten(), **S_evaluations_view_config
-)
+if control_points_shown:
+    cp_x = np.array(surfaces)[:, :, :, ix].flatten()  # control points x-coordinates
+    cp_y = np.array(surfaces)[:, :, :, iy].flatten()  # control points y-coordinates
+    cp_z = np.array(surfaces)[:, :, :, iz].flatten()  # control points z-coordinates
 
-u, t = np.meshgrid(S.evaluation_times_u, S.evaluation_times_t)
-u, t = u.flatten(), t.flatten()
-tri = mtri.Triangulation(u, t)
-S_triangulation_view_config = dict(alpha=0.8, color="tab:blue", triangles=tri.triangles)
-ax.plot_trisurf(
-    surf_x.flatten(), surf_y.flatten(), surf_z.flatten(), **S_triangulation_view_config
-)
+    ax.plot3D(cp_x, cp_y, cp_z, **vbsp.defaults["control_points_kwargs"])
+
+for control_points, surface_color in zip(surfaces, vbsp.colors):
+    S = bsp.Surface(
+        kv_t, kv_u, control_points, degree_t, degree_u, n_bisections=nbi, verbose=True,
+    )
+    (surf_x, surf_y, surf_z) = S.evaluations
+
+    ax.plot3D(
+        surf_x.flatten(),
+        surf_y.flatten(),
+        surf_z.flatten(),
+        **vbsp.defaults["evaluation_points_kwargs"],
+    )
+
+    u, t = np.meshgrid(S.evaluation_times_u, S.evaluation_times_t)
+    u, t = u.flatten(), t.flatten()
+    tri = mtri.Triangulation(u, t)
+
+    triangulation_kwargs = dict(triangles=tri.triangles)
+    triangulation_kwargs.update(**vbsp.defaults["surface_kwargs"])
+
+    current_color_kwargs = dict(color=surface_color)
+    triangulation_kwargs.update(current_color_kwargs)
+
+    ax.plot_trisurf(
+        surf_x.flatten(), surf_y.flatten(), surf_z.flatten(), **triangulation_kwargs
+    )
 
 # Back to the singleton figure
 plt.show()
+
+if serialize:
+    extension = ".pdf"
+    filename = Path(__file__).name + extension
+    fig.savefig(filename, bbox_inches="tight", pad_inches=0)
+    print(f"Serialized to {filename}")
