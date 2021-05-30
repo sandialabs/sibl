66d531e8227a7e47d96f193b9d980f4e44ecbdcd
Chad Hovey <chovey@sandia.gov>
2021-05-30 00:19:12 -0600

    finalize first B-spline basis functions

--- geo/doc/plot_bspline_N0_p0_to_p4.py
+++ geo/doc/plot_bspline_N0_p0_to_p4.py
@@ -11,9 +11,9 @@ import ptg.bspline as bsp
 # degree = 0
 display = True
 dpi = 100  # dots per inch
-latex = False
-serialize = False
-verbose = False
+latex = True  # False
+n_bisections = 4  # 2^n_bisections
+serialize = True  # False
 
 colors = ("tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple", "tab:cyan")
 linestyles = ("solid", "dashed", "dashdot")
@@ -22,23 +22,22 @@ if latex:
     rc("font", **{"family": "serif", "serif": ["Computer Modern Roman"]})
     rc("text", usetex=True)
 
-i = 0
-# knot_vector = (0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0)  # num_knots = 7
-# degree = 1  # constant
-degrees = (0, 1, 2)  # constant, linear, quadratic, etc.
-# knot_vector = tuple(map(float, np.arange(degree + 2)))
-# knot_vector = (0, 0, 1, 2, 2)
-knot_vectors = ((0, 1), (0, 0, 1, 2, 2), (0, 0, 0, 1, 2, 3, 3, 3))
-# coef = (0.0, 1.0, 0.0, 0.0, 0.0, 0.0)
-# coef = (0.0, 1.0, 0.0, 0.0, 0.0)
-# coef = (1.0, 0.0, 0.0, 0.0, 0.0)
-coefs = ((1.0,), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0, 0.0, 0.0))
-# coef = (
-#     (0.0,),
-#     (1.0,),
-#     (0.0,),
-# )
-labels = ("$N_0^0$", "$N_0^1$", "$N_0^2$")
+degrees = (0, 1, 2, 3, 4)  # constant, linear, quadratic, etc.
+knot_vectors = (
+    (0, 1),
+    (0, 0, 1, 2, 2),
+    (0, 0, 0, 1, 2, 3, 3, 3),
+    (0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4),
+    (0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 5, 5, 5, 5),
+)
+coefs = (
+    (1.0,),
+    (0.0, 1.0, 0.0),
+    (0.0, 0.0, 1.0, 0.0, 0.0),
+    (0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0),
+    (0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0),
+)
+labels = ("$N_0^0$", "$N_0^1$", "$N_0^2$", "$N_0^3$", "$N_0^4$")
 
 # fig = plt.figure(figsize=plt.figaspect(1.0 / (len(knot_vector) - 1)), dpi=dpi)
 fig = plt.figure(figsize=plt.figaspect(1.0 / max(knot_vectors[-1])), dpi=dpi)
@@ -57,35 +56,28 @@ for i in np.arange(len(degrees)):
     N0_p = bsp.Curve(knot_vector, coef, degree)
     result = N0_p.is_valid()
 
-    n_bisections = 3  # 2^n_bisections
     npts = int((knot_vector[-1] - knot_vector[0]) * (2 ** n_bisections) + 1)
     # tmin, tmax, npts = knot_vector[0], knot_vector[-1], 13
     tmin, tmax, npts = knot_vector[0], knot_vector[-1], npts
 
     t = np.linspace(tmin, tmax, npts, endpoint=True)
     y = N0_p.evaluate(t)
-    # y_known = (0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-
-    # fig = plt.figure(figsize=plt.figaspect(1.0 / (len(knot_vector) - 1)), dpi=dpi)
-    # ax = fig.gca()
-    # # ax.grid()
-    # ax.grid(True, which="major", linestyle="-")
-    # ax.grid(True, which="minor", linestyle=":")
-    # ax.scatter(t, y)
     ax.plot(
         t,
         y,
-        "-*",
+        "-",
         linewidth=2,
-        # label="$N_{0}^{" + str(i) + "}",
         label=label,
         linestyle=linestyles[np.remainder(i, len(linestyles))],
     )
 
 ax.set_xlabel(r"$t$")
-# ax.set_ylabel(r"$N^{" + str(degree) + "}_{" + str(k) + "}(t)$")
 ax.set_ylabel(r"$N^{p\in[0,4]}_0(t)$")
-ax.legend(bbox_to_anchor=(1.05, 1), loc="upper left", borderaxespad=0.0)
+ax.legend(loc="upper right")
+
+_eps = 0.1
+ax.set_xlim([knot_vectors[-1][0] - 2 * _eps, knot_vectors[-1][-1] + 2 * _eps])
+ax.set_ylim([0.0 - 2 * _eps, 1.0 + 2 * _eps])
 
 ax.xaxis.set_major_locator(MultipleLocator(1.0))
 ax.xaxis.set_minor_locator(MultipleLocator(0.25))
