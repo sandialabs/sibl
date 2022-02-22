c3b356ae5c4e36ef78ef44192029a759a3ef41b8
Chad Hovey <chovey@sandia.gov>
2022-02-22 13:37:37 -0700

    hello world for c++ with vscode

--- geo/src/dual/main.cpp
+++ geo/src/dual/main.cpp
@@ -7,6 +7,7 @@ enabled with the -std=c++11 or -std=gnu++11 compiler options.
 #include <iostream>
 #include <tuple>
 #include <cstdlib>
+
 #include "NodeList.h"
 #include "Curve.h"
 #include "QuadTree.h"
@@ -23,8 +24,7 @@ int main(int argc, char *argv[])
 {
     Parser myParser;
 
-
-    if(argc >= 2)
+    if (argc >= 2)
     {
         double minsize;
         string filename;
@@ -32,12 +32,12 @@ int main(int argc, char *argv[])
         int bnds;
         int featureRefine;
         bool developerOutput = true;
-        std::tuple<double,double,double> ll,ur;
+        std::tuple<double, double, double> ll, ur; // lower left, upper right
 
         const std::string compilation_date = __DATE__;
-        cout<<"Build Date : "<<compilation_date<<endl;
+        cout << "Build Date : " << compilation_date << endl;
 
-        if(argc ==2)
+        if (argc == 2)
         {
             myParser.file(argv[1]);
 
@@ -68,14 +68,12 @@ int main(int argc, char *argv[])
             featureRefine = atoi(argv[4]);
         }
 
+        cout << "Filename : " << filename << endl;
+        cout << "Resolution: " << minsize << endl;
+        cout << "Bnds: " << bnds << endl;
+        cout << "Refine on the feature only: " << featureRefine << endl;
 
-        cout<<"Filename : "<<filename<<endl;
-        cout<<"Resolution: "<<minsize<<endl;
-        cout<<"Bnds: "<<bnds<<endl;
-        cout<<"Refine on the feature only: "<<featureRefine<<endl;
-
-
-        Curve* myCurve;
+        Curve *myCurve;
         try
         {
             myCurve = new Curve(filename);
@@ -85,36 +83,36 @@ int main(int argc, char *argv[])
             cout << "Runtime error: " << e.what();
             return -1;
         }
-        cout<<"Done loading and processing curve."<<endl;
-        filename = filename.substr(0,filename.length()-4);
+        cout << "Done loading and processing curve." << endl;
+        filename = filename.substr(0, filename.length() - 4);
 
-        //if(developerOutput)
-        //myCurve->write("line");
-        if(bnds == 1)
+        // if(developerOutput)
+        // myCurve->write("line");
+        if (bnds == 1)
         {
-            cout<<"Using unit bounds"<<endl;
-            myCurve->lowerLeft(std::tuple<double,double>(-1,-1));
-            myCurve->upperRight(std::tuple<double,double>(1,1));
+            cout << "Using unit bounds" << endl;
+            myCurve->lowerLeft(std::tuple<double, double>(-1, -1));
+            myCurve->upperRight(std::tuple<double, double>(1, 1));
         }
-        else if(argc==2)
+        else if (argc == 2)
         {
-            ///parser specified boundaries
-            cout<<"Using parser bounds"<<endl;
-            myCurve->lowerLeft(std::tuple<double,double>(std::get<0>(ll),std::get<1>(ll)));
-            myCurve->upperRight(std::tuple<double,double>(std::get<0>(ur),std::get<1>(ur)));
+            /// parser specified boundaries
+            cout << "Using parser bounds" << endl;
+            myCurve->lowerLeft(std::tuple<double, double>(std::get<0>(ll), std::get<1>(ll)));
+            myCurve->upperRight(std::tuple<double, double>(std::get<0>(ur), std::get<1>(ur)));
         }
         else
-            cout<<"Using tight bounding box"<<endl;
+            cout << "Using tight bounding box" << endl;
 
-        cout<<"Lower bounds: "<<get<0>(ll)<<", "<<get<1>(ll)<<endl;
-        cout<<"Upper bounds: "<<get<0>(ur)<<", "<<get<1>(ur)<<endl;
-        NodeList* myNodes;
+        cout << "Lower bounds: " << get<0>(ll) << ", " << get<1>(ll) << endl;
+        cout << "Upper bounds: " << get<0>(ur) << ", " << get<1>(ur) << endl;
+        NodeList *myNodes;
         myNodes = new NodeList();
-        //cout<<"Nodes size: "<<myNodes->size()<<endl;
-        QuadTree* myQuadTree;
+        // cout<<"Nodes size: "<<myNodes->size()<<endl;
+        QuadTree *myQuadTree;
         try
         {
-            myQuadTree=new QuadTree(myCurve,myNodes,minsize);
+            myQuadTree = new QuadTree(myCurve, myNodes, minsize);
         }
         catch (std::runtime_error e)
         {
@@ -124,72 +122,67 @@ int main(int argc, char *argv[])
 
         myQuadTree->subdivide(myQuadTree->head());
 
+        // cout<<"Nodes size: "<<myNodes->size()<<endl;
 
-
-        //cout<<"Nodes size: "<<myNodes->size()<<endl;
-
-
-        myQuadTree->balancedRefineCurve(myQuadTree->head(),(featureRefine!=1));
-        cout<<"Nodes size: "<<myNodes->size()<<endl;
-        if(developerOutput)
-            myQuadTree->write(outputfile+"_01_quad_tree_");
+        myQuadTree->balancedRefineCurve(myQuadTree->head(), (featureRefine != 1));
+        cout << "Nodes size: " << myNodes->size() << endl;
+        if (developerOutput)
+            myQuadTree->write(outputfile + "_01_quad_tree_");
 
         myQuadTree->assignSplitCode(myQuadTree->head());
 
-        Primal* myPrimal = new Primal(myQuadTree);
+        Primal *myPrimal = new Primal(myQuadTree);
 
-        if(developerOutput)
-            myPrimal->write(outputfile+"_02_primal_","");
+        if (developerOutput)
+            myPrimal->write(outputfile + "_02_primal_", "");
 
-        cout<<"Dual"<<endl;
-        Dual* myDual;
+        cout << "Dual" << endl;
+        Dual *myDual;
         try
         {
             myDual = new Dual(myPrimal);
         }
-        catch(std::runtime_error e)
+        catch (std::runtime_error e)
         {
             std::cout << "Runtime error: " << e.what();
             return -1;
         }
 
-        cout<<"traverse done"<<endl;
-        if(developerOutput)
-            myDual->write(outputfile+"_03_dual_","");
-        if(bnds == 0)
+        cout << "traverse done" << endl;
+        if (developerOutput)
+            myDual->write(outputfile + "_03_dual_", "");
+        if (bnds == 0)
         {
             myDual->trim();
-            if(developerOutput)
-                myDual->write(outputfile+"_04_d_trim_","");
+            if (developerOutput)
+                myDual->write(outputfile + "_04_d_trim_", "");
             myDual->project();
-            if(developerOutput)
-                myDual->write(outputfile+"_05_dt_project_","");
-            cout<<"projection done"<<endl;
+            if (developerOutput)
+                myDual->write(outputfile + "_05_dt_project_", "");
+            cout << "projection done" << endl;
             myDual->snap();
-            if(developerOutput)
-                myDual->write(outputfile+"_06_dtp_snap_","");
-            cout<<"snap done"<<endl;
+            if (developerOutput)
+                myDual->write(outputfile + "_06_dtp_snap_", "");
+            cout << "snap done" << endl;
             myDual->subdivide();
-            if(developerOutput)
-                myDual->write(outputfile+"_07_dtps_subdivide_","");
-            cout<<"subdivide done"<<endl;
+            if (developerOutput)
+                myDual->write(outputfile + "_07_dtps_subdivide_", "");
+            cout << "subdivide done" << endl;
             myDual->project();
-            if(developerOutput)
-                myDual->write(outputfile+"_08_dtpss_project_","");
+            if (developerOutput)
+                myDual->write(outputfile + "_08_dtpss_project_", "");
             myDual->snap();
-            if(developerOutput)
-                myDual->write(outputfile+"_09_dtpssp_snap_","");
+            if (developerOutput)
+                myDual->write(outputfile + "_09_dtpssp_snap_", "");
 
-            if(developerOutput)
-                myDual->write(outputfile+"_10_mesh_","");
+            if (developerOutput)
+                myDual->write(outputfile + "_10_mesh_", "");
 
             myDual->updateActiveNodes();
-            myDual->write(outputfile,"inp");
+            myDual->write(outputfile, "inp");
         }
 
-
-        cout<<"execution done"<<endl;
-
+        cout << "execution done" << endl;
     }
     else
     {
@@ -201,58 +194,56 @@ int main(int argc, char *argv[])
          cout<< myParser.boolVal("boundary_refine_flag:")<<endl;
          cout<< get<0>(myParser.lowerVal("bounding_box:"))<< ", "<<get<1>(myParser.lowerVal("bounding_box:"))<<", "<<get<2>(myParser.lowerVal("bounding_box:"))<<endl;
          cout<< get<0>(myParser.upperVal("bounding_box:"))<< ", "<<get<1>(myParser.upperVal("bounding_box:"))<<", "<<get<2>(myParser.upperVal("bounding_box:"))<<endl;
-         *////Run some tests:
-        Node A,B;
+         */
+        /// Run some tests:
+        Node A, B;
         B.X(1);
         B.Y(1);
 
         Node direction = A.direction(B);
         double dist = A.dist(B);
-        cout<<direction<<endl;
-        cout<<dist<<endl;
+        cout << direction << endl;
+        cout << dist << endl;
 
-        Curve* myCurve = new Curve();
+        Curve *myCurve = new Curve();
 
-        myCurve->lowerLeft(std::tuple<double,double>(-1,-1));
-        myCurve->upperRight(std::tuple<double,double>(1,1));
+        myCurve->lowerLeft(std::tuple<double, double>(-1, -1));
+        myCurve->upperRight(std::tuple<double, double>(1, 1));
 
-        std::tuple<double,double,double> T1(0,0,0);
-        std::tuple<double,double,double> T2(1,0,0);
-        std::tuple<double,double,double> T3(1,1,0);
-        std::tuple<double,double,double> T4(0,1,0);
-        std::tuple<double,double,double> P1(0,.5,0);
-        std::tuple<double,double,double> P2(1,.5,0);
-        std::tuple<double,double,double> P3(0,1.5,0);
-        std::tuple<double,double,double> P4(1,1.5,0);
-        std::tuple<double,double,double> P5(0,0,0);
-        std::tuple<double,double,double> P6(1,0,0);
-        std::tuple<double,double,double> P7(1,1,0);
-        std::tuple<double,double,double> P8(0,1,0);
-        std::tuple<double,double,double> P9(1,2,0);
-        std::tuple<double,double,double> P10(0,-1,0);
-        std::tuple<double,double,double> P11(1,0,0);
+        std::tuple<double, double, double> T1(0, 0, 0);
+        std::tuple<double, double, double> T2(1, 0, 0);
+        std::tuple<double, double, double> T3(1, 1, 0);
+        std::tuple<double, double, double> T4(0, 1, 0);
+        std::tuple<double, double, double> P1(0, .5, 0);
+        std::tuple<double, double, double> P2(1, .5, 0);
+        std::tuple<double, double, double> P3(0, 1.5, 0);
+        std::tuple<double, double, double> P4(1, 1.5, 0);
+        std::tuple<double, double, double> P5(0, 0, 0);
+        std::tuple<double, double, double> P6(1, 0, 0);
+        std::tuple<double, double, double> P7(1, 1, 0);
+        std::tuple<double, double, double> P8(0, 1, 0);
+        std::tuple<double, double, double> P9(1, 2, 0);
+        std::tuple<double, double, double> P10(0, -1, 0);
+        std::tuple<double, double, double> P11(1, 0, 0);
 
+        cout << "intersection (true): " << myCurve->intersects(T1, T2, T3, P1, P2) << endl;
+        cout << "intersection (false): " << myCurve->intersects(T1, T2, T3, P3, P4) << endl;
+        cout << "intersection (true): " << myCurve->intersects(T1, T2, T3, P5, P6) << endl;
+        cout << "intersection (true): " << myCurve->intersects(T1, T2, T3, P5, P7) << endl;
+        cout << "intersection (false): " << myCurve->intersects(T1, T2, T3, P8, P9) << endl;
+        cout << "intersection (true): " << myCurve->intersects(T1, T2, T3, P10, P11) << endl;
 
-        cout<<"intersection (true): "<<myCurve->intersects(T1,T2,T3,P1,P2)<<endl;
-        cout<<"intersection (false): "<<myCurve->intersects(T1,T2,T3,P3,P4)<<endl;
-        cout<<"intersection (true): "<<myCurve->intersects(T1,T2,T3,P5,P6)<<endl;
-        cout<<"intersection (true): "<<myCurve->intersects(T1,T2,T3,P5,P7)<<endl;
-        cout<<"intersection (false): "<<myCurve->intersects(T1,T2,T3,P8,P9)<<endl;
-        cout<<"intersection (true): "<<myCurve->intersects(T1,T2,T3,P10,P11)<<endl;
-
-
-
-        NodeList* myNodes;
+        NodeList *myNodes;
         myNodes = new NodeList();
 
-        QuadTree* myQuadTree;
-        myQuadTree=new QuadTree(myCurve,myNodes,1);
+        QuadTree *myQuadTree;
+        myQuadTree = new QuadTree(myCurve, myNodes, 1);
         myQuadTree->subdivide(myQuadTree->head());
         myQuadTree->assignSplitCode(myQuadTree->head());
-        Primal* myPrimal = new Primal(myQuadTree);
-        myPrimal->write("primal","");
-        Dual* myDual = new Dual(myPrimal);
-        myDual->write("dual","");
+        Primal *myPrimal = new Primal(myQuadTree);
+        myPrimal->write("primal", "");
+        Dual *myDual = new Dual(myPrimal);
+        myDual->write("dual", "");
 
         /*
                 Surface* mySurface;
@@ -273,9 +264,6 @@ int main(int argc, char *argv[])
                 cout<<"Nodes size: "<<myNodes3->size()<<endl;
                 //myNodes3->print();
         */
-
-
-
     }
     return 0;
 }
---
+++ geo/src/hwcpp/README.md
@@ -0,0 +1,67 @@
+# Getting Started with C/C++ for Visual Studio Code
+
+## Install
+
+### Visual Studio Code
+
+* In VS Code, install the C/C++ by Microsoft extension
+  * Also install
+    * C/C++ Extension Pack
+    * C/C++ Themes
+
+### Compiler
+
+```bash
+g++ --version  # for g++
+clang -- version ## for clang
+
+g++ --version
+Configured with: --prefix=/Applications/Xcode.app/Contents/Developer/usr --with-gxx-include-dir=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/4.2.1
+Apple clang version 12.0.0 (clang-1200.0.32.29)
+Target: x86_64-apple-darwin19.6.0
+Thread model: posix
+InstalledDir: /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin
+
+> clang --version
+Apple clang version 12.0.0 (clang-1200.0.32.29)
+Target: x86_64-apple-darwin19.6.0
+Thread model: posix
+InstalledDir: /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin
+```
+
+Assure the binaries are in the `$PATH`:
+
+```bash
+> echo $PATH
+/Users/chovey/opt/miniconda3/envs/siblenv/bin /Users/chovey/opt/miniconda3/condabin /Users/sparta/tools/nvim-osx64/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /Library/Apple/usr/bin /Library/TeX/texbin
+
+> pwd
+/usr/bin
+> ls -flt g++
+-rwxr-xr-x  1 root  wheel  31488 Sep 21  2020 g++*
+> ls -flt clang
+-rwxr-xr-x  1 root  wheel  31488 Sep 21  2020 clang*
+```
+
+### Debugger
+
+We will use `lldb` since `gdb` is not installed on the Mac yet.
+
+```bash
+which lldb                                                             (siblenv)
+/usr/bin/lldb
+which gdb                                                              (siblenv)
+>
+```
+
+* Run menu, Add Configuration... item
+
+For Linux and Mac, use the `GDB` debugger.  
+[Configure](https://code.visualstudio.com/docs/cpp/launch-json-reference) VS Code 
+for C/C++ debugging.
+
+## References
+
+* [VS Code docs](https://code.visualstudio.com/docs/languages/cpp)
+* [VS Code debugger](https://code.visualstudio.com/docs/cpp/cpp-debug)
+* [Debug a C++ project in VS Code](https://youtu.be/G9gnSGKYIg4)
