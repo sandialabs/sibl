e1784615d5adc2bb41deee32f05c37f610870562
Chad Hovey <chovey@sandia.gov>
2022-05-05 11:25:32 -0600

    more verbose style shell script

--- style.sh
+++ style.sh
@@ -3,6 +3,9 @@
 # -----
 # Black
 # -----
+echo "---------------------------------"
+echo "Checking code style with Black..."
+echo "---------------------------------"
 black --check . --diff
 # black --check . --diff --line-length=79
 
@@ -19,10 +22,18 @@ black --check . --diff
 # flake8 . --statistics
 # E501 - line length exceeds 79 characters
 # https://peps.python.org/pep-0008/#maximum-line-length
+echo "----------------------------------"
+echo "Checking code style with flake8..."
+echo "----------------------------------"
 flake8 --ignore E203,E501,W503 . --statistics
 
 # ----
 # mypi
 # ----
 # https://github.com/python/mypy
+
+echo "-----------------------------"
+echo "The script has now completed."
+echo "-----------------------------"
+
 
