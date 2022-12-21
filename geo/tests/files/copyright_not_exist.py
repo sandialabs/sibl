8e520c1c6bcc7f9bb372d775638916242f02081a
Chad Hovey <hovey@me.com>
2022-12-21 16:46:38 -0700

    pass Black

--- geo/tests/files/copyright_not_exist.py
+++ geo/tests/files/copyright_not_exist.py
@@ -1,6 +1,7 @@
 # This is a simple test python module that does
 # not contain the copyright block.
 
+
 def hello() -> str:
     """This is a simple dummy Python code that does a simple function."""
     return "Hello World!"
