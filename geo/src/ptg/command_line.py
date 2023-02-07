6460c9a4a93db623f602d38454205c9e54ed2f1d
Chad B. Hovey <hovey@me.com>
2023-02-07 12:22:40 -0700

    incremental add copyright

--- geo/src/ptg/command_line.py
+++ geo/src/ptg/command_line.py
@@ -16,3 +16,18 @@ def version() -> str:
     ver = pkg_resources.require("ptg")[0].version
     print("SIBL Mesh Engine, ptg package version:")
     return ver
+
+
+"""
+Copyright 2023 Sandia National Laboratories
+
+Notice: This computer software was prepared by National Technology and Engineering Solutions of
+Sandia, LLC, hereinafter the Contractor, under Contract DE-NA0003525 with the Department of Energy
+(DOE). All rights in the computer software are reserved by DOE on behalf of the United States
+Government and the Contractor as provided in the Contract. You are authorized to use this computer
+software for Governmental purposes but it is not to be released or distributed to the public.
+NEITHER THE U.S. GOVERNMENT NOR THE CONTRACTOR MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES
+ANY LIABILITY FOR THE USE OF THIS SOFTWARE. This notice including this sentence must appear on any
+copies of this computer software. Export of this data may require a license from the United States
+Government.
+"""
